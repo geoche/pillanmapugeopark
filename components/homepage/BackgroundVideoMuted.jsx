d4532abf500fd9f004bf7a7fbeb6f54944105b49
HeorhiiCheban <4georgecheban@gmail.com>
2024-08-08 18:12:57 +0200

    Adjust negative margin in BackgroundVideoMuted component
    Reduced the top margin from -9% to -8.5% for better positioning. This change fine-tunes the layout alignment on the homepage for improved visual presentation.

--- components/homepage/BackgroundVideoMuted.jsx
+++ components/homepage/BackgroundVideoMuted.jsx
@@ -4,7 +4,7 @@ import pilMapLogo from "@/public/assets/images/logo.png";
 
 const BackgroundVideoMuted = () => {
     return (
-        <div className={`relative w-full h-auto -mt-[9%]`}>
+        <div className={`relative w-full h-auto -mt-[8.5%]`}>
             <div
                 className={`absolute flex flex-center flex-col items-center text-white text-sm md:text-lg left-0 right-0 top-0 bottom-0 justify-center`}>
                 <p>GEOPARK</p>
