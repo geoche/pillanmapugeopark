9b515bbce19d4f50c42799a8330fb0631ad5f172
HeorhiiCheban <4georgecheban@gmail.com>
2024-09-20 14:42:24 +0200

    Remove unnecessary margin class in NavbarHomeLogo
    The "m-1" margin class in the Link component was redundant and has been removed. This change helps in maintaining cleaner code and preventing unintended spacing issues.

--- components/navbar/NavbarHomeLogo.jsx
+++ components/navbar/NavbarHomeLogo.jsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 const NavbarHomeLogo = ({lang}) => {
     return (
-        <Link href={`/${lang}`} className="m-1 my-2 w-[15%] max-w-20">
+        <Link href={`/${lang}`} className="my-2 w-[15%] max-w-20">
             <div className="relative ">
                 <Image
                     src={logo}
                     alt="logo"
-                    className={`opacity-75 w-full h-full`}/>
+                    className={`w-full h-full`}/>
             </div>
         </Link>
     );
