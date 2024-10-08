9d489ab9035c16e654a51487c259df5c338855e1
HeorhiiCheban <4georgecheban@gmail.com>
2024-10-08 10:54:32 +0200

    Update image paths in georoutes component
    Moved image paths to a more specific georoutes directory for consistency. This change ensures all georoutes images are organized in the designated folder, improving maintainability and clarity.

--- components/explore/georoutes/georoutes.js
+++ components/explore/georoutes/georoutes.js
@@ -2,7 +2,7 @@ export const georoutes = [
     {
         id:"exploreGeopark.georoutes.georoute.tenorRiverMountainValley.uri",
         title: "exploreGeopark.georoutes.georoute.tenorRiverMountainValley.title",
-        mainImgSrc: "/assets/images/explore/tenorRiverMountainValley.jpg",
+        mainImgSrc: "/assets/images/explore/georoutes/tenorRiverMountainValley.jpg",
         videoId: "1VONshSCNRk",
         sectionText: {
             par1:"exploreGeopark.georoutes.georoute.tenorRiverMountainValley.sectionText.par1",
@@ -13,7 +13,7 @@ export const georoutes = [
     {
         id:"exploreGeopark.georoutes.georoute.radalSieteTazas.uri",
         title: "exploreGeopark.georoutes.georoute.radalSieteTazas.title",
-        mainImgSrc: "/assets/images/explore/radalSieteTazas.jpg",
+        mainImgSrc: "/assets/images/explore/georoutes/radalSieteTazas.jpg",
         videoId: "LlOAtubo54k",
         sectionText: {
             par1:"exploreGeopark.georoutes.georoute.radalSieteTazas.sectionText.par1",
@@ -23,7 +23,7 @@ export const georoutes = [
     {
         id:"exploreGeopark.georoutes.georoute.altosDeLircay.uri",
         title: "exploreGeopark.georoutes.georoute.altosDeLircay.title",
-        mainImgSrc: "/assets/images/explore/altosDeLircay.jpg",
+        mainImgSrc: "/assets/images/explore/georoutes/altosDeLircay.jpg",
         videoId: "L3uTFvDaX3o",
         sectionText: {
             par1:"exploreGeopark.georoutes.georoute.altosDeLircay.sectionText.par1",
@@ -34,7 +34,7 @@ export const georoutes = [
     {
         id:"exploreGeopark.georoutes.georoute.pehuencheInternationalPass.uri",
         title: "exploreGeopark.georoutes.georoute.pehuencheInternationalPass.title",
-        mainImgSrc: "/assets/images/explore/pehuencheInternationalPass.jpg",
+        mainImgSrc: "/assets/images/explore/georoutes/pehuencheInternationalPass.jpg",
         videoId: "VEXeD46jHnA",
         sectionText: {
             par1: "exploreGeopark.georoutes.georoute.pehuencheInternationalPass.sectionText.par1",
@@ -46,7 +46,7 @@ export const georoutes = [
     {
         id:"exploreGeopark.georoutes.georoute.meladoMountainValley.uri",
         title: "exploreGeopark.georoutes.georoute.meladoMountainValley.title",
-        mainImgSrc: "/assets/images/explore/meladoMountainValley.jpg",
+        mainImgSrc: "/assets/images/explore/georoutes/meladoMountainValley.jpg",
         videoId: "TzA8srm4m38",
         sectionText: {
             par1: "exploreGeopark.georoutes.georoute.meladoMountainValley.sectionText.par1",
@@ -56,7 +56,7 @@ export const georoutes = [
     {
         id:"exploreGeopark.georoutes.georoute.artisanTraditionOfRari.uri",
         title: "exploreGeopark.georoutes.georoute.artisanTraditionOfRari.title",
-        mainImgSrc: "/assets/images/explore/artisanTraditionOfRari.jpg",
+        mainImgSrc: "/assets/images/explore/georoutes/artisanTraditionOfRari.jpg",
         videoId: "0H5tbBrAQqg",
         sectionText: {
             par1: "exploreGeopark.georoutes.georoute.artisanTraditionOfRari.sectionText.par1",
@@ -67,7 +67,7 @@ export const georoutes = [
     {
         id:"exploreGeopark.georoutes.georoute.archibuenoRiverMountainValley.uri",
         title: "exploreGeopark.georoutes.georoute.archibuenoRiverMountainValley.title",
-        mainImgSrc: "/assets/images/explore/archibuenoRiverMountainValley.jpg",
+        mainImgSrc: "/assets/images/explore/georoutes/archibuenoRiverMountainValley.jpg",
         videoId: "5bexe4Yhels",
         sectionText: {
             par1: "exploreGeopark.georoutes.georoute.archibuenoRiverMountainValley.sectionText.par1",
